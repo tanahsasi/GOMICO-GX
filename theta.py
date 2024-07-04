0ad6c2985e27117f32cdbd1822edd6d169abddd2
tanahsasi <nkhsha031009@g.neec.ac.jp>
2024-07-04 15:40:35 +0900

    cosθのプログラム作成
    プログラムを作成した

--- theta.py
+++ theta.py
@@ -7,3 +7,20 @@ pxtheta = rad/240
 print(pxtheta)
 pxk = mt.degrees(pxtheta)
 print(pxk)
+
+i=1
+cos0 = np.arange(480 ,dtype=float)
+cos=np.cos(rad)
+cos0[0] = cos
+print(cos0[0])
+r=rad-pxtheta
+while 480>i:
+    print(cos0[i-1])
+    print(r)
+    if 239>i:
+        r=r-pxtheta
+    else:
+        r=r+pxtheta
+    cos=np.cos(r)
+    cos0[i]=cos
+    i = i+1
